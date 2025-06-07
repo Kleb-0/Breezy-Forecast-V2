205b52b439eb35f1bc201d9676ca2866bcacf49a
Kleb-0 <shadowhilles@gmail.com>
2025-06-07 10:07:54 -0300

    refactor: change color palette from dark to earthy pastel

--- src/components/searchfield.jsx
+++ src/components/searchfield.jsx
@@ -2,12 +2,12 @@ import { Search } from "lucide-react";
 
 export default function SearchField() {
   return (
-    <div className="bg-neutral-50 text-neutral-950 rounded-2xl h-8 relative w-64 px-1.5 pt-1 text-center">
-      <Search className="absolute left-3 text-neutral-800" />
+    <div className="bg-neutral-50 rounded-2xl h-8 relative w-64 px-1.5 pt-1 text-center">
+      <Search className="absolute left-3 text-orange-900" />
       <input
         type="search"
         placeholder="Search for a place"
-        className="w-full pl-10 bg-transparent flex-1 outline-none text-neutral-950"
+        className="w-full pl-10 bg-transparent flex-1 outline-none text-orange-900"
       />
     </div>
   );
