8641d70fa47f88187190265c2f1ebe595a55b675
Kleb-0 <shadowhilles@gmail.com>
2025-06-08 13:09:26 -0300

    refactor: adjust layout in CurrentWeather component

--- src/components/currentWeather.jsx
+++ src/components/currentWeather.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function CurrentWeather(){
       return (
-        <div className="flex justify-around sm:justify-center">
-          <div className="flex flex-col justify-center items-center sm:pr-8">
+        <div className="flex justify-center">
+          <div className="flex flex-col justify-center items-center sm:pr-9">
             <h1 className="font-bold text-5xl">14°C</h1>
             <p>High: 18°C</p>
             <p>Low: 7°C</p>
           </div>
-          <div className="flex items-center justify-center sm:pl-8">
+          <div className="flex items-center justify-center sm:pl-9">
             <Image
               src="/sun-between-clouds.png"
               alt="weather icon"
