742f602dddbcf9991c2db0b49574866e535b0f2e
Kleb-0 <shadowhilles@gmail.com>
2025-06-13 21:59:17 -0300

    feat: add useFetch hook for data fetching

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!url) return;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+}
+
+export default useFetch;
