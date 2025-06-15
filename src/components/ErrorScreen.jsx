47b9859604236d47172c852c0b847c12090173ce
Kleb-0 <shadowhilles@gmail.com>
2025-06-15 13:43:07 -0300

    feat: add ErrorScreen component for displaying fetch errors

---
+++ src/components/ErrorScreen.jsx
@@ -0,0 +1,7 @@
+export default function ErrorScreen() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-2xl">Error fetching weather, please try again</h1>
+    </div>
+  );
+}
