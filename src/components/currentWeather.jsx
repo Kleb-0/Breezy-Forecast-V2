fbc5d4847c85728eadb0fbc59efe3bf25d1eaf81
Kleb-0 <shadowhilles@gmail.com>
2025-06-15 13:48:17 -0300

    feat: update CurrentWeather component to display dynamic weather data

--- src/components/currentWeather.jsx
+++ src/components/currentWeather.jsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
 
-export default function CurrentWeather(){
-      return (
-        <div className="flex justify-center">
-          <div className="flex flex-col justify-center items-center sm:pr-9">
-            <h1 className="font-bold text-5xl">14°C</h1>
-            <p>High: 18°C</p>
-            <p>Low: 7°C</p>
-          </div>
-          <div className="flex items-center justify-center sm:pl-9">
-            <Image
-              src="/sun-between-clouds.png"
-              alt="weather icon"
-              width={130}
-              height={130}
-            />
-          </div>
-        </div>
-      );
+export default function CurrentWeather({ currentWeatherData }) {
+  const { currentTemperature, maxTemperature, minTemperature, weatherIcon } =
+    currentWeatherData;
+
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col justify-center items-center sm:pr-9">
+        <h1 className="font-bold text-5xl">{currentTemperature}°C</h1>
+        <p>High: {maxTemperature}°C</p>
+        <p>Low: {minTemperature}°C</p>
+      </div>
+      <div className="flex items-center justify-center sm:pl-9">
+        <Image
+          src="/sun-between-clouds.png"
+          alt="weather icon"
+          width={130}
+          height={130}
+        />
+      </div>
+    </div>
+  );
 }
