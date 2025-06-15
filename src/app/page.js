bc2d6f601e4dfe4f6e0701b0cc4ef549e7d8ed0f
Kleb-0 <shadowhilles@gmail.com>
2025-06-15 13:46:38 -0300

    feat: implement loading and error handling in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
+"use client";
+
 import CurrentWeather from "@/components/currentWeather";
+import ErrorScreen from "@/components/ErrorScreen";
 import Forecast from "@/components/forecast";
+import LoadingScreen from "@/components/LoadingScreen";
+import useFilterCurrentData from "@/hooks/useFilterCurrentData";
 
 export default function Home() {
+  const currentWeatherData = useFilterCurrentData();
+
+  if (currentWeatherData.isLoading) {
+    return <LoadingScreen />;
+  }
+
+  if (currentWeatherData.error) {
+    return <ErrorScreen />;
+  }
+
   return (
     <div>
-      <CurrentWeather />
+      <CurrentWeather currentWeatherData={currentWeatherData} />
       <Forecast />
     </div>
   );
