4a4dc4e143b65ada99e44ceb982eb34f32e03a9a
Calebe Hillesheim Lamb <shadowhilles@gmail.com>
2025-10-16 16:18:37 -0300

    refactor: improved responsiveness in ErrorScreen

--- src/components/ErrorScreen.jsx
+++ src/components/ErrorScreen.jsx
@@ -5,9 +5,9 @@ export default function ErrorScreen() {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col justify-center items-center bg-[var(--foreground)] rounded-xl shadow-lg p-8 animate-fade-in w-fit">
-        <h1 className="text-2xl font-bold text-white mb-2">No city named "{city}" Found</h1>
-        <p className="text-white text-lg opacity-80">Please check the spelling and try again.</p>
+      <div className="flex flex-col justify-center items-center bg-[var(--foreground)] rounded-xl shadow-lg p-8 mx-8 animate-fade-in w-fit">
+        <h1 className="text-lg sm:text-2xl font-bold text-white mb-2">No city named "{city}" Found</h1>
+        <p className="text-white text-sm sm:text-lg opacity-80">Please check the spelling and try again.</p>
       </div>
     </div>
   );
