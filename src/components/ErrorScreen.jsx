094987dc217c106381721160fef3d5c5784b4ac9
Calebe Hillesheim Lamb <shadowhilles@gmail.com>
2025-10-16 16:05:07 -0300

    feat: update ErrorScreen component to display specific city error message and improve styling

--- src/components/ErrorScreen.jsx
+++ src/components/ErrorScreen.jsx
@@ -1,7 +1,14 @@
+import { useCity } from "@/context/WeatherProvider";
+
 export default function ErrorScreen() {
+  const { city } = useCity();
+
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-2xl">Error fetching weather, please try again</h1>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center items-center bg-[var(--foreground)] rounded-xl shadow-lg p-8 animate-fade-in w-fit">
+        <h1 className="text-2xl font-bold text-white mb-2">No city named "{city}" Found</h1>
+        <p className="text-white text-lg opacity-80">Please check the spelling and try again.</p>
+      </div>
     </div>
   );
 }
