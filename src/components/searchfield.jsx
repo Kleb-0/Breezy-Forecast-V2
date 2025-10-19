81bd8159fb398a63b56aeb8ec44556d296b7a084
Calebe Hillesheim Lamb <shadowhilles@gmail.com>
2025-10-19 18:47:13 -0300

    refactor: replace default style of the SearchField component

--- src/components/searchfield.jsx
+++ src/components/searchfield.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCity } from "@/context/WeatherProvider";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 
 export default function SearchField() {
   const { city, setCity } = useCity();
@@ -11,15 +11,20 @@ export default function SearchField() {
   }
 
   return (
-    <div className="bg-neutral-50 rounded-2xl h-8 relative w-64 px-1.5 pt-1 text-center">
+    <div className="bg-neutral-50 rounded-2xl h-8 py-1 relative w-64 px-1.5 pt-1 text-center items-center flex">
       <Search className="absolute left-3 text-orange-900" />
       <input
         type="search"
         onChange={catchInputValue}
         value={city}
         placeholder="Search for a place"
-        className="w-full pl-10 bg-transparent flex-1 outline-none text-orange-900"
+        className="w-full pl-10 bg-transparent outline-none text-orange-900"
       />
+      {city && (
+        <button onClick={() => setCity("")} className="absolute right-3">
+          <X />
+        </button>
+      )}
     </div>
   );
 }
