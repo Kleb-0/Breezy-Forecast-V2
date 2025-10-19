b16333fd6fde11c4d0730aa0a88b982b50b94a79
Calebe Hillesheim Lamb <shadowhilles@gmail.com>
2025-10-19 18:59:11 -0300

    refactor: initialize city state with an empty string instead of default value

--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -5,7 +5,7 @@ import { useContext, createContext, useState } from "react";
 const WeatherContext = createContext();
 
 export default function WeatherProvider({ children }) {
-  const [city, setCity] = useState("Lucerna");
+  const [city, setCity] = useState("");
 
   return (
     <WeatherContext.Provider value={{ city, setCity }}>
